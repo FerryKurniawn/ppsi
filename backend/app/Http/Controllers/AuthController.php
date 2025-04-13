ccb26946a450102a75d6b5867a378aaf772f2e3b
MFathurrochim <faturohimskd06@gmail.com>
2025-04-13 20:38:15 +0700

    membuat backend untuk database user dan backend login

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    // Fungsi untuk login pengguna
+    public function login(Request $request)
+    {
+        // Validasi input
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+
+        $user = User::where('username', $request->username)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        // Jika login berhasil
+        return response()->json(['message' => 'Login successful', 'user' => $user], 200);
+    }
+}
