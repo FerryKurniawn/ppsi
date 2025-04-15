31db3722a742cf888ce0d7e3aa09635195417085
MFathurrochim <faturohimskd06@gmail.com>
2025-04-15 18:35:42 +0700

    buat rekap surat keluar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Suratmasuk from "./components/Surat-masuk";
-import Suratkeluar from "./components/Surat-keluar"
+import Suratkeluar from "./components/Surat-keluar";
+import RekapSuratMasuk from "./components/Rekap-surat-masuk";
+import RekapSuratKeluar from "./components/Rekap-surat-keluar";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/surat-masuk" element={<Suratmasuk />} />
           <Route path="/surat-keluar" element={<Suratkeluar />} />
+          <Route path="/Rekap-surat-masuk" element={<RekapSuratMasuk />} />
+          <Route path="/Rekap-surat-keluar" element={<RekapSuratKeluar />} />
         </Routes>
       </BrowserRouter>
     </>
